1b621940824e3504cfdc7e98fa9e28c066b87614
Moeyua <mail@moeyua.com>
2026-01-08 18:42:40 +0800

    feat: 更新 SiteTitle 组件的样式，修改主题配置中的副标题，优化布局和全局样式

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -1,3 +1,47 @@
-import { defaultConfig } from '@/config/default'
+import type { ThemeConfig } from './types/theme'
 
-export const themeConfig = defaultConfig
+export const themeConfig: ThemeConfig = {
+  site: {
+    author: 'Moeyua',
+    description: 'Rediscory the beauty of typography',
+    website: 'https://typography.moeyua.com/',
+    title: '講評世界',
+    subtitle: 'Moeyua',
+    socialLinks: [
+      {
+        name: 'github',
+        href: 'https://github.com/moeyua',
+      },
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+      {
+        name: 'twitter',
+        href: 'https://twitter.com/moeyua',
+      },
+      {
+        name: 'mastodon',
+        href: 'https://mastodon.social/@moeyua',
+      },
+    ],
+    navigationLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+    ],
+  },
+}
