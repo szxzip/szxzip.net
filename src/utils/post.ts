98452d2e2cee9173f3a1a91d91bf2f0339334300
Moeyua <mail@moeyua.com>
2026-01-09 15:53:17 +0800

    feat: 更新 PostSummary 组件以使用 getPostUrl 函数；重命名 fetchPostEntries 为 getPostEntries；添加动态路由支持；新增文章内容

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,6 +1,11 @@
+import type { PostEntry } from '@/types/post'
 import { getCollection } from 'astro:content'
 
-export async function fetchPostEntries() {
+export async function getPostEntries() {
   const postsEntries = await getCollection('posts')
   return postsEntries
+}
+
+export function getPostUrl(postEntry: PostEntry) {
+  return `/post/${postEntry.id}/`
 }
