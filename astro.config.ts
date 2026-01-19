18705b9a43981a27f859003da51b3323594ffbca
Moeyua <mail@moeyua.com>
2026-01-19 16:52:15 +0800

    feat: 更新配置文件，添加 prefetch 和 base 设置

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,8 @@ import { themeConfig } from '@/theme.config'
 // https://astro.build/config
 export default defineConfig({
   site: themeConfig.site.website,
+  prefetch: true,
+  base: '/',
   integrations: [
   ],
   vite: {
