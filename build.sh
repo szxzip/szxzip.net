9e1dd1c9e02aa5283813778bc14ed5b7a3e02280
szxzip <szxzip@example.com>
2026-02-10 21:58:13 +0800

    0

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull
-rm -rf /var/www/html/*
-docker run --rm \
-  -v $(pwd):/src \
-  -v /var/www/html:/src/public \
-  hugomods/hugo:exts \
-  --minify --gc --cleanDestinationDir --logLevel debug
